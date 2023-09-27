7aa564491c46b5b1f7b2fe9f92e321bcc6e9e3c7
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-09-27 20:35:54 +0200

    Initial Commit

---
+++ Calculate!/Calculate_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculate_App.swift
+//  Calculate!
+//
+//  Created by Håkon Sunde on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Calculate_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculate!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculate!
+//
+//  Created by Håkon Sunde on 27/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
